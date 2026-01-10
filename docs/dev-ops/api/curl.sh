232578cc5bb576dfae378bd22c1868560f6af689
fantasy <384130598@qq.com>
2026-01-10 21:26:01 +0800

    feature:云服务配置ollama

--- docs/dev-ops/api/curl.sh
+++ docs/dev-ops/api/curl.sh
@@ -1,7 +1,7 @@
-curl http://117.72.115.188:11434/api/generate \
+curl http://111.228.62.219:11434/api/generate \
   -H "Content-Type: application/json" \
   -d '{
         "model": "deepseek-r1:1.5b",
-        "prompt": "1+1",
+        "prompt": "who are yuo?",
         "stream": false
       }'
