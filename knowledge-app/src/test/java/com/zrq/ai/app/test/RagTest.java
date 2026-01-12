2726e1ff6e72248b8b5355872dd7d8ede1803a09
fantasy <384130598@qq.com>
2026-01-12 21:05:55 +0800

    feature:ragTest

---
+++ knowledge-app/src/test/java/com/zrq/ai/app/test/RagTest.java
@@ -0,0 +1,89 @@
+package com.zrq.ai.app.test;
+
+import com.alibaba.fastjson.JSON;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author zrq
+ * @time 2026/1/12 19:45
+ * @description
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RagTest {
+
+    @Resource
+    private OllamaChatClient chatClient;
+
+    @Resource
+    private TokenTextSplitter tokenTextSplitter;
+
+    @Resource
+    private SimpleVectorStore simpleVectorStore;
+
+    @Resource
+    private PgVectorStore pgVectorStore;
+
+    @Test
+    public void update() {
+        TikaDocumentReader reader = new TikaDocumentReader("./doc/Rag.txt");
+        List<Document> documents = reader.get();
+        List<Document> documentList = tokenTextSplitter.apply(documents);
+        documents.forEach(document -> document.getMetadata().put("knowledge", "rag1"));
+        documentList.forEach(document -> document.getMetadata().put("knowledge", "rag1"));
+        pgVectorStore.accept(documentList);
+        log.info("end!!");
+    }
+
+    @Test
+    public void chat() {
+        String message = "我需要李铁柱的部分信息。";
+
+        String SYSTEM_PROMPT = """
+                Use the information from the DOCUMENTS section to provide accurate answers but act as if you knew this information innately.
+                If unsure, simply state that you don't know.
+                Another thing you need to note is that your reply must be in Chinese!
+                DOCUMENTS:
+                    {documents}
+                """;
+
+        SearchRequest request = SearchRequest.query(message).withTopK(5).withFilterExpression("knowledge == 'rag2'");
+
+        List<Document> documents = pgVectorStore.similaritySearch(request);
+        String documentsCollectors = documents.stream().map(Document::getContent).collect(Collectors.joining());
+
+        Message ragMessage = new SystemPromptTemplate(SYSTEM_PROMPT).createMessage(Map.of("documents", documentsCollectors));
+
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(new UserMessage(message));
+        messages.add(ragMessage);
+
+        ChatResponse chatResponse = chatClient.call(new Prompt(messages, OllamaOptions.create().withModel("deepseek-r1:1.5b")));
+
+        log.info("测试结果:{}", JSON.toJSONString(chatResponse));
+    }
+}
